6caf60b9baf8aa631dafea5fd2c40d7cb1ed46f3
Raul <akshay.raul@accenture.com>
2022-02-03 15:12:52 +0100

    Commenting unecessary logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,10 @@ export class AppComponent {
     this.displayCountParities.push({ category: "Start - Number" ,  pointer: timerData });
 
     if (timerData % 2 === 0) {
-      console.log("even");
+      // console.log("even");
       this.displayCountParities.push({ category: "Even - Number" ,  pointer: timerData });
     } else {
-      console.log("odd");
+      // console.log("odd");
       this.displayCountParities.push({ category: "Odd - Number" ,  pointer: timerData });
     }
 
